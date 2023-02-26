0b05f9932fd22ed244af20d1bf59fc04be97e35c
charleskan <71898304+charleskan@users.noreply.github.com>
2023-02-26 22:03:28 +0800

    Initial Commit

---
+++ new/newApp.swift
@@ -0,0 +1,17 @@
+//
+//  newApp.swift
+//  new
+//
+//  Created by Yowaki on 26/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct newApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
